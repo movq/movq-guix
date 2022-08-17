f50c1981b9ab1f0b7f0ff05a1de2a5ad42427e47
Mike Jones <mike@mjones.io>
2022-08-17 20:28:43 +0100

    Add custom kernel.

--- movq/packages/linux.scm
+++ movq/packages/linux.scm
@@ -1,9 +1,22 @@
 (define-module (movq packages linux)
+  #:use-module (nongnu packages linux)
   #:use-module (gnu packages)
+  #:use-module (gnu packages compression)
   #:use-module (gnu packages linux)
   #:use-module (guix gexp)
   #:use-module (guix git-download)
-  #:use-module (guix packages))
+  #:use-module (guix packages)
+  #:use-module (srfi srfi-1))
+
+(define-public linux-movq-xps13
+  (package
+    (inherit linux)
+    (name "linux-movq-xps13")
+    (native-inputs
+      `(("kconfig" ,(local-file "files/xps13.config"))
+        ("zstd" ,zstd)
+       ,@(alist-delete "kconfig"
+                       (package-native-inputs linux))))))
 
 (define-public pipewire-current
   (package
