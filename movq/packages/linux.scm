836c89e325db673e7bcc1f93016d0f9c2b021a33
Mike Jones <mike@mjones.io>
2022-09-10 17:17:36 +0100

    Add Alienware kernel

--- movq/packages/linux.scm
+++ movq/packages/linux.scm
@@ -3,6 +3,7 @@
   #:use-module (gnu packages)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages linux)
+  #:use-module (gnu packages python)
   #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module (guix packages)
@@ -15,6 +16,18 @@
     (native-inputs
       `(("kconfig" ,(local-file "files/xps13.config"))
         ("zstd" ,zstd)
+       ,@(alist-delete "kconfig"
+                       (package-native-inputs linux))))))
+
+(define-public linux-alienware
+  (package
+    (inherit linux)
+    (name "linux-alienware")
+    (native-inputs
+      `(("kconfig" ,(local-file "files/alienware.config"))
+        ("zstd" ,zstd)
+        ("zlib" ,zlib)
+        ("python" ,python)
        ,@(alist-delete "kconfig"
                        (package-native-inputs linux))))))
 
