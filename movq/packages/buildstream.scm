14ea892255c0759b8426e5c5d0f779d35201bcec
Mike Jones <mike@mjones.io>
2022-09-27 16:40:12 +0100

    wip

--- movq/packages/buildstream.scm
+++ movq/packages/buildstream.scm
@@ -2,14 +2,23 @@
   #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix packages)
+  #:use-module (guix build-system cmake)
   #:use-module (guix build-system python)
   #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages adns)
   #:use-module (gnu packages check)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages cpp)
+  #:use-module (gnu packages logging)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages protobuf)
   #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages regex)
   #:use-module (gnu packages rpc)
   #:use-module (gnu packages serialization)
   #:use-module (gnu packages time)
+  #:use-module (gnu packages tls)
   #:use-module (gnu packages virtualization)
   )
 
@@ -65,3 +74,52 @@
   (description
    "This package provides a framework for modelling build pipelines in YAML")
   (license #f)))
+
+(define-public buildbox-common
+  (package
+    (name "buildbox-common")
+    (version "0.0.62")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+               (url "https://gitlab.com/BuildGrid/buildbox/buildbox-common")
+               (commit version)))
+        (file-name (git-file-name name version))
+        (sha256
+          (base32 "1y58wnn4glphbm68anm96sinzv1158kggyxv41pf40qdrc6rz33r"))))
+    (build-system cmake-build-system)
+    (native-inputs (list googletest pkg-config))
+    (inputs (list abseil-cpp
+                  c-ares
+                  glog
+                  grpc
+                  openssl
+                  protobuf
+                  re2
+                  `(,util-linux "lib")
+                  zlib))
+    (home-page #f)
+    (synopsis #f)
+    (description #f)
+    (license #f)))
+
+(define-public buildbox-casd
+  (package
+    (name "buildbox-casd")
+    (version "0.0.62")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+               (url "https://gitlab.com/BuildGrid/buildbox/buildbox-casd.git")
+               (commit version)))
+        (file-name (git-file-name name version))
+        (sha256
+          (base32 "1yxmbsihsib35vdj1pxhpbnx3i0a9zjg10zf7kndgrd2378gnsdg"))))
+    (build-system cmake-build-system)
+    (inputs (list buildbox-common))
+    (home-page #f)
+    (synopsis #f)
+    (description #f)
+    (license #f)))
