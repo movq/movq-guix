72639d81d7e917867e550bc57861984643c4c07c
Mike Jones <mike@mjones.io>
2022-10-29 12:08:17 +0100

    Use rust-current to build firefox

--- movq/packages/mozilla.scm
+++ movq/packages/mozilla.scm
@@ -159,5 +159,5 @@
         (prepend lld-wrapper)
         (replace "clang" clang-14)
         (replace "llvm" llvm-14)
-        (replace "rust" rust-1.63)
+        (replace "rust" rust-current)
         (delete "wasm32-wasi-clang-toolchain")))))
