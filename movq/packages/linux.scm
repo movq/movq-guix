b55ff7226a44970c9053e2a531964016fc365d50
Mike Jones <mike@mjones.io>
2023-01-27 11:50:41 +0000

    Remove custom pipewire/wireplumber

--- movq/packages/linux.scm
+++ movq/packages/linux.scm
@@ -30,37 +30,3 @@
         ("python" ,python)
        ,@(alist-delete "kconfig"
                        (package-native-inputs linux))))))
-
-(define-public pipewire-current
-  (package
-    (inherit pipewire-0.3)
-    (name "pipewire")
-    (version "0.3.56")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/PipeWire/pipewire")
-                    (commit version)))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "066g4ri2k8pdflclvr2919f6x98gmqrqyj1xyiingw2nn2pwgcf1"))))))
-
-(define-public wireplumber-current
-  (package
-    (inherit wireplumber)
-    (name "wireplumber")
-    (version "0.4.12")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url
-              "https://gitlab.freedesktop.org/pipewire/wireplumber.git")
-             (commit version)))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "0k6ylqq4r1xzc7bbmwy1qgwpqra8shdvilwm1pvzzvilgyqkm8ys"))))
-    (inputs (modify-inputs (package-inputs wireplumber)
-              (replace "pipewire" pipewire-current)
-              (delete "elogind")))))
