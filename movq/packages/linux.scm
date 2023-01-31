f50ed1abea3ee530e48c9c8a4fbcf6ab88717f16
Mike Jones <mike@mjones.io>
2023-01-31 15:22:24 +0000

    Use new way of customising kernel

--- movq/packages/linux.scm
+++ movq/packages/linux.scm
@@ -10,23 +10,9 @@
   #:use-module (srfi srfi-1))
 
 (define-public linux-movq-xps13
-  (package
-    (inherit linux)
-    (name "linux-movq-xps13")
-    (native-inputs
-      `(("kconfig" ,(local-file "files/xps13.config"))
-        ("zstd" ,zstd)
-       ,@(alist-delete "kconfig"
-                       (package-native-inputs linux))))))
+  (customize-linux #:linux linux
+                   #:deconfig ,(local-file "files/xps13.config")))
 
 (define-public linux-alienware
-  (package
-    (inherit linux)
-    (name "linux-alienware")
-    (native-inputs
-      `(("kconfig" ,(local-file "files/alienware.config"))
-        ("zstd" ,zstd)
-        ("zlib" ,zlib)
-        ("python" ,python)
-       ,@(alist-delete "kconfig"
-                       (package-native-inputs linux))))))
+  (customize-linux #:linux linux
+                   #:deconfig ,(local-file "files/alienware.config")))
